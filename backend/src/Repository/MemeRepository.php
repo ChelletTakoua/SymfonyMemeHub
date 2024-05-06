0201f697c67501582fb97c88afe0c53004f7ec51
medtaher <56443200+medtaher123@users.noreply.github.com>
2024-05-06 02:01:41 +0100

    implemented MemeRepository

--- backend/src/Repository/MemeRepository.php
+++ backend/src/Repository/MemeRepository.php
@@ -5,14 +5,11 @@ namespace App\Repository;
 use App\Entity\Meme;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-
+//do not remove/overwride the find() method or it will break the code in the controller
 /**
  * @extends ServiceEntityRepository<Meme>
  *
  * @method Meme|null find($id, $lockMode = null, $lockVersion = null)
- * @method Meme|null findOneBy(array $criteria, array $orderBy = null)
- * @method Meme[]    findAll()
- * @method Meme[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MemeRepository extends ServiceEntityRepository
 {
@@ -20,31 +17,134 @@ class MemeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Meme::class);
     }
-    /**
-     * Finds all memes in the repository, sorted by their creation date in ascending order.
-     *
-     * @return array An array of Meme entities, sorted by their creation date in ascending order.
-     */
-    public function findAllByDateASC(){
-        return $this->createQueryBuilder('m')
-            ->orderBy('m.creationDate', 'ASC')
+    
+
+    /*public function findRandomMeme(): ?Meme
+    {
+        return $this->getMemeBaseQuery()
+            ->orderBy('RAND()')
+            ->setMaxResults(1)
             ->getQuery()
-            ->getResult()
-        ;
-    }
-    /**
-     * Finds all memes in the repository, sorted by their creation date in descending order.
-     *
-     * @return array An array of Meme entities, sorted by their creation date in descending order.
-     */
-    public function findAllByDateDESC(){
-        return $this->createQueryBuilder('m')
-            ->orderBy('m.creationDate', 'DESC')
+            ->getOneOrNullResult();
+    }*/
+
+
+    public function findAll(bool $includeBlocked = false): array
+    {
+        return $this->getMemeBaseQuery($includeBlocked)
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
 
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null,bool $includeBlocked = false): array
+    {
+        $queryBuilder = $this->getMemeBaseQuery($includeBlocked);
+
+        foreach ($criteria as $field => $value) {
+            $queryBuilder->andWhere("m.$field = :$field")
+                ->setParameter($field, $value);
+        }
+
+        if ($orderBy) {
+            foreach ($orderBy as $sort => $order) {
+                $queryBuilder->addOrderBy("m.$sort", $order);
+            }
+        }
+
+        if ($limit) {
+            $queryBuilder->setMaxResults($limit);
+        }
+
+        if ($offset) {
+            $queryBuilder->setFirstResult($offset);
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+
+    private function getMemeBaseQuery(bool $includeBlocked = false){
+        $queryBuilder = $this->createQueryBuilder('m');
+
+        if (!$includeBlocked) {
+            $queryBuilder->leftJoin('App\Entity\BlockedMeme', 'bm', 'WITH', 'm.id = bm.meme')
+                ->where('bm.meme IS NULL');
+        }
+
+        return $queryBuilder;
+    }
+
+    public function findOneBy(array $criteria, array $orderBy = null,bool $includeBlocked = false): ?Meme
+    {
+        $queryBuilder = $this->getMemeBaseQuery($includeBlocked);
+
+        foreach ($criteria as $field => $value) {
+            $queryBuilder->andWhere("m.$field = :$field")
+                ->setParameter($field, $value);
+        }
+
+        if ($orderBy) {
+            foreach ($orderBy as $sort => $order) {
+                $queryBuilder->addOrderBy("m.$sort", $order);
+            }
+        }
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
+
+    /**
+     *  This method returns memes paginated.
+     *
+     * @param int $page The page number to return.
+     * @param int $pageSize The number of memes to return per page. If -1 is passed, all memes will be returned.
+     * @param bool $includeBlocked If true, blocked memes will be included in the results.
+     * @return Meme[]
+     *
+     */
+    public function findPaginated(int $page = 1, int $pageSize = -1, bool $includeBlocked = false): array
+    {
+        if ($page < 1) {
+            throw new \InvalidArgumentException('Page number cannot be less than 1.');
+        }
+
+        if ($pageSize == -1) {
+            return $this->findAll($includeBlocked);
+        }
+
+        $offset = ($page - 1) * $pageSize;
+
+        return $this->getMemeBaseQuery($includeBlocked)
+            ->setFirstResult($offset)
+            ->setMaxResults($pageSize)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function memeIsBlocked(int $memeId): bool
+    {
+        return $this->createQueryBuilder('m')
+            ->select('COUNT(bm.id)')
+            ->leftJoin('App\Entity\BlockedMeme', 'bm', 'WITH', 'm.id = bm.meme')
+            ->where('m.id = :memeId')
+            ->setParameter('memeId', $memeId)
+            ->getQuery()
+            ->getSingleScalarResult() > 0;
+    }
+
+//    /**
+//     * @return Meme[] Returns an array of Meme objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('m.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
 //    public function findOneBySomeField($value): ?Meme
 //    {
