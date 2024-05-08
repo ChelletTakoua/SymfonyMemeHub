100f562ab749c7c2ba8bcfc31af75dba5c3dd6b6
takoua174 <taki74ayadi@gmail.com>
2024-05-08 14:53:28 +0100

    meme repo

--- backend/src/Repository/MemeRepository.php
+++ backend/src/Repository/MemeRepository.php
@@ -158,7 +158,29 @@ class MemeRepository extends ServiceEntityRepository
     {
         return $this->findBy(['user' => $userId], ['creationDate' => 'DESC'], null, null, $includeBlocked);
     }
+    public function findByASC(){
+        return $this->createQueryBuilder('m')
+            ->orderBy('m.creationDate', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    public function findbyDESC(){
+        return $this->createQueryBuilder('m')
+            ->orderBy('m.creationDate', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
+    public function findBydate($date){
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.creationDate = :val')
+            ->setParameter('val', $date)
+            ->getQuery()
+            ->getResult()
+        ; 
+    }
 
 
 
